2fd60192a393a0232847d4b6b9e0dd5e9878e84f
Radu Barbuta <radubarbuta@Radus-MacBook-Pro.local>
2016-11-06 19:32:33 -0700

    Git Commit Test on GitHub

---
+++ testRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testRepository
+//
+//  Created by Radu Barbuta on 2016-11-06.
+//  Copyright © 2016 Radu Barbuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
